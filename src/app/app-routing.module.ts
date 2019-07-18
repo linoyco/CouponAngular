34db4aca1f630430ba20123c18b6590ade98dd2f
Linoy Cohen <45672703+linoyco@users.noreply.github.com>
2019-07-18 12:24:54 +0300

    newww changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './components/admin/admin.component';
 import { CustomerComponent } from './components/customer/customer.component';
 import { CompanyComponent } from './components/company/company.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   { path: "admin", component: AdminComponent},
   { path: "customer", component: CustomerComponent},
-  { path: "company", component: CompanyComponent}
+  { path: "company", component: CompanyComponent},
+  { path: "login", component: LoginComponent}
 ];
 
 @NgModule({
