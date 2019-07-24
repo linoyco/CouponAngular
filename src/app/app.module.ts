b8abff8557a05f1a9909de3d07dca8a356d9f2a5
Linoy Cohen <45672703+linoyco@users.noreply.github.com>
2019-07-24 14:57:48 +0300

    method components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,16 @@ import { LoginComponent } from './components/login/login.component';
 import { RootPageComponent } from './components/root-page/root-page.component';
 import { HeaderRootComponent } from './components/root-page/header-root/header-root.component';
 import { AsideRootComponent } from './components/root-page/aside-root/aside-root.component';
+import { CreateCompanyComponent } from './components/admin/create-company/create-company.component';
+import { RemoveCompanyComponent } from './components/admin/remove-company/remove-company.component';
+import { UpdateCompanyComponent } from './components/admin/update-company/update-company.component';
+import { GetCompanyComponent } from './components/admin/get-company/get-company.component';
+import { GetAllCompaniesComponent } from './components/admin/get-all-companies/get-all-companies.component';
+import { CreateCustomerComponent } from './components/admin/create-customer/create-customer.component';
+import { RemoveCustomerComponent } from './components/admin/remove-customer/remove-customer.component';
+import { UpdateCustomerComponent } from './components/admin/update-customer/update-customer.component';
+import { GetCustomerComponent } from './components/admin/get-customer/get-customer.component';
+import { GetAllCustomersComponent } from './components/admin/get-all-customers/get-all-customers.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +29,16 @@ import { AsideRootComponent } from './components/root-page/aside-root/aside-root
     RootPageComponent,
     HeaderRootComponent,
     AsideRootComponent,
+    CreateCompanyComponent,
+    RemoveCompanyComponent,
+    UpdateCompanyComponent,
+    GetCompanyComponent,
+    GetAllCompaniesComponent,
+    CreateCustomerComponent,
+    RemoveCustomerComponent,
+    UpdateCustomerComponent,
+    GetCustomerComponent,
+    GetAllCustomersComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
---
+++ src/app/components/admin/get-all-companies/get-all-companies.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-get-all-companies',
+  templateUrl: './get-all-companies.component.html',
+  styleUrls: ['./get-all-companies.component.css']
+})
+export class GetAllCompaniesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
