d687db294e7ee134265cc102025f8f277abb2bc1
Linoy Cohen <45672703+linoyco@users.noreply.github.com>
2019-08-07 22:09:11 +0300

    connection to server

---
+++ src/app/services/connection-service.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+import { LoginUser } from '../components/login/LoginUser';
+
+@Injectable({providedIn: 'root'})
+
+export class ConnectionServiceService {
+
+  private _urlLogin = 'http://localhost:8080/CouponSystemREST/rest/LoginService/login';
+
+  constructor(private http: HttpClient) { 
+
+  }
+
+  login(user: LoginUser){
+    return this.http.post<any>(this._urlLogin, user).pipe(
+      catchError(
+        (err: HttpErrorResponse)=>{
+          console.log(err)
+          return throwError('error in http login')
+        }
+      )
+
+    )
+  }
+}
