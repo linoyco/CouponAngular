659b3ae004b086f8f8c8d3180133560c6e404eef
Linoy Cohen <45672703+linoyco@users.noreply.github.com>
2019-09-30 17:57:33 +0300

    navigate ofter create company

--- src/app/components/admin/create-company/create-company.component.ts
+++ src/app/components/admin/create-company/create-company.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CompanyBeanService } from 'src/app/services/company-bean.service';
 import { ResponseCodes } from 'src/app/models/responseCodeEnums';
 import { HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-company',
@@ -10,13 +11,13 @@ import { HttpErrorResponse } from '@angular/common/http';
 })
 export class CreateCompanyComponent implements OnInit {
 
-  constructor(private companyBeanService: CompanyBeanService) { }
+  constructor(private companyBeanService: CompanyBeanService,private router: Router) { }
 
   ngOnInit() {
   }
 
 
-  private addCompany(name, password, email){
+  private createCompany(name, password, email){
     this.companyBeanService.addCompany(name, password, email).subscribe(res => {
       if(res.status === ResponseCodes.OK){ console.log(res.body); alert("success to create company!") }
       else{ console.log("something wrong");}
@@ -26,6 +27,7 @@ export class CreateCompanyComponent implements OnInit {
       if(resError.error === ResponseCodes.UNAUTHORIZED){ console.log("session expired");}
       else { console.log("something wrong with this new company !"); alert("this company already exist!");}
     });
+    this.router.navigate(["/admin"])
   }
 
 }
